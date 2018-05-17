eed38f05fbc3a952ac5c1438388885af6fb36c4f
Mar, Mike <31870347+Mikegmar@users.noreply.github.com>
2018-05-16 19:10:53 -0700

    added info

--- CIT360/cit360/JavaCollection/hashSet.java
+++ CIT360/cit360/JavaCollection/hashSet.java
@@ -1,5 +1,19 @@
 package JavaCollection;
 
-public class hasSet {
+public class hashSet {
 
+	HashSet<String> set = new HashSet<String>();   
+    
+	  // Adding Guns to set  
+	  set.add("Remington");  
+	  set.add("Smith & Wesson");  
+	  set.add("Remington"); // This adds a duplicate. In sets that isn't allowed
+	  set.add("Beretta");
+	  set.add("Smith & Wesson"); // Ditto. Won't add duplicates.
+	  set.add("Smith & Wesson");
+	  
+	  System.out.println("\nSet example\n");
+	  // Display set items 
+	  for(String gunSet:set){  
+			System.out.println(gunSet); 
 }
